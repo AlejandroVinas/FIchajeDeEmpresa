abaa18e22bfd47b17dc51f80e36abf11e1c0c9c4
vicbita <vicbita2006@gmail.com>
2026-04-20 11:22:37 +0200

    Eliminar proyecto anterior y dejar solo la solución .NET

--- flutter_app/lib/config.dart
+++
@@ -1,9 +0,0 @@
-class AppConfig {
-  // ← Cambia por la IP/dominio real del servidor
-  static const String apiUrl = 'http://192.168.1.100:3000';
-
-  // Deben coincidir exactamente con los valores en .env del backend
-  static const double empresaLat  = 40.416775;
-  static const double empresaLon  = -3.703790;
-  static const double radioMetros = 100;
-}
